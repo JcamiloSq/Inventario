d4ea2fbccebc03bc060a83f7ff15b0fcdedf906b
Camilo Echavarria <yilmar-eq-64@hotmail.com>
2024-05-12 19:14:49 -0500

    Merge branch 'dev' of https://github.com/JcamiloSq/Inventario into dev

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter , Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter , Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import 'react-notifications/lib/notifications.css';
 import { NotificationContainer } from 'react-notifications';
 import App from './App';
 import Login from './pages/login/login';
 import Dashboard from './pages/dashboard/Dashboard';
+import { Box, CssBaseline, Toolbar } from '@mui/material';
+import RoutesApp from './routes/Routes';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+
 const ProtectedRoute = ({ children }) => {
   const user = localStorage.getItem('user');
 
@@ -17,13 +18,56 @@ const ProtectedRoute = ({ children }) => {
 
   return children;
 };
+
+const BlockedRoute = ({ children }) => {
+  const user = localStorage.getItem('user');
+
+  if (user) {
+    return <Navigate to="/Dashboard" />;
+  }
+
+  return children;
+};
+
+const Layout = () => {
+  const location = useLocation();
+  const hideOnRoutes = ['/', '/login'];
+  const showComponents = !hideOnRoutes.includes(location.pathname);
+  const user = localStorage.getItem('user');
+
+  if (!user) {
+    return (
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path='*' element={<RoutesApp/>}></Route>
+        </Routes>
+    );
+  } else {
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        {showComponents && <Dashboard />}
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          {showComponents && <Toolbar />}
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/Dashboard" />} />
+            <Route path="/login" element={<BlockedRoute><Login /></BlockedRoute>} />
+            <Route path="*" element={<RoutesApp />}/>
+          </Routes>
+        </Box>
+      </Box>
+    );
+  }
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Dashboard"
-        element={ <ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+      <Route path="*" element={<Layout />} />
     </Routes>
     <NotificationContainer />
   </BrowserRouter>
